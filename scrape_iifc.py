04f2075edbb681640ffb0998efdf5401c3a1b639
kaichogami <asishrocks95@gmail.com>
2017-11-26 19:03:32 +0530

    fixed update

--- scrape_iifc.py
+++ scrape_iifc.py
@@ -4,6 +4,7 @@ import pandas as pd
 from os import listdir
 from os.path import isfile, join
 import sqlite3
+from pandas.util.testing import assert_frame_equal
 
 URL = "https://www.rbi.org.in/scripts/bs_viewcontent.aspx?Id=2009"
 
@@ -35,19 +36,43 @@ def get_excels(links):
 
 
 def check_changes(old_df, new_df):
+    import numpy as np
     if len(old_df) != len(new_df):
         return ('length', new_df)
 
     # check if their data has changed in any way
     # if it has, return the new_df
 
-    if old_df.sort_values('IFSC').equals(new_df.sort_values('IFSC')):
+    old_df = old_df.sort_values('IFSC')    
+    new_df = new_df.sort_values('IFSC')
+    old_df.index = new_df.index
+    #old_df.fillna(value='-1', inplace=True)
+    #new_df.fillna(value='-1', inplace=True)
+    new_df.iloc[:, 5] = new_df.iloc[:, 5].astype(float)
+    #print(type(old_df.iloc[0, 5]), type(new_df.iloc[0,5]))
+
+    old_df =old_df.applymap(unicode)
+    new_df = new_df.applymap(unicode)
+    #print(type(old_df.iloc[0, 5]), type(new_df.iloc[0,5]))
+    #print(assert_frame_equal(old_df, new_df, check_dtype=False))
+    #print(check_df(old_df, new_df))
+    if check_df(old_df, new_df):
         return ('no_change', old_df)
     else:
         return ('value_change', new_df)
 
+def check_df(old_df, new_df):
+    for col in old_df.columns:
+        if list(old_df[col] == new_df[col]).count(False) >0:
+           return False
+
+    if old_df['BANK'].iloc[0] == "ANDHRA PRAGATHI GRAMEENA BANK":
+        pass
+
+    return True
+            
 def save_database(df, table_name, db):
-    df.to_sql(table_name, db)
+    df.to_sql(table_name, db, if_exists="replace")
 
 def combine(df_list):
     return pd.concat(df_list)
@@ -55,7 +80,7 @@ def combine(df_list):
 def get_df_from_db(db, table_name, bank_name):
     df = pd.read_sql_query("SELECT * FROM {0} WHERE BANK = '{1}';".format(table_name,
                                                                        bank_name),
-                           db)
+                           db, coerce_float=False)
     return df
 
 def get_branch(db, table_name, ifsc):
@@ -63,8 +88,8 @@ def get_branch(db, table_name, ifsc):
     cur.execute("SELECT * FROM {0} WHERE IFSC = '{1}';".format(table_name,
                                                            ifsc))
     result = cur.fetchall()
-    print(result)
-    return
+    return result
+
 
 if __name__ == '__main__':
     DO = raw_input('What needs to be done?\n1)create\n2)update\n3)query\n')
@@ -73,6 +98,7 @@ if __name__ == '__main__':
     if DO == 'create':
         all_df = get_excels([])
         one_df = combine(all_df)
+        one_df = one_df.applymap(unicode)
 
         # create sqlite3 connection and save data
         conn = sqlite3.connect(db_name)
@@ -84,21 +110,23 @@ if __name__ == '__main__':
         all_df = get_excels([])
         change_list = []
         for index, new_df in enumerate(all_df):
-            old_df = get_df_from_db(conn, table_name, new_df.iloc['BANK'])
+            old_df = get_df_from_db(conn, table_name, new_df.iloc[0]['BANK'])
+            old_df.drop('index', axis=1, inplace=True)
             return_values = check_changes(old_df, new_df)
             all_df[index] = return_values[1]
             change_list.append(return_values[0])
 
         #check if change_list has all no_change value
+        print(change_list)
         if change_list.count('no_change') == len(change_list):
             print('no change, nothing to update')
         else:
+            print("saw changes")
             save_database(combine(all_df), table_name, conn)
         conn.close()
 
     elif DO =='query':
         conn = sqlite3.connect(db_name)
         ifsc = raw_input('enter ifsc')
-        get_branch(conn, table_name, ifsc)
-
-
+        print(get_branch(conn, table_name, ifsc))
+        conn.close()
